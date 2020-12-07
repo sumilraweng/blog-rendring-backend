c89803d33dd069392661be9206fc84fe3e5e3ba0
sumilraweng <sumil.jain@raweng.com>
2020-12-07 19:59:00 +0530

    This is the first commit to blog backend rendering

---
+++ README.md
@@ -0,0 +1,51 @@
+# Blog Rendering backend
+
+This is Nodejs-express project. Which is implemented to learn the fundamentals of express. In this project we make the server using express and having api
+
+- /blogs - to fetch the all blogs
+- /blogs/:id - to fetch blog with respective id
+
+## folder structure
+
+```
+├── app.js
+├── DATA
+│   └── blog.json
+├── package.json
+└── README.md
+```
+
+## Installation
+
+Install the dependencies and start the server.
+
+### prerequisite
+
+1. vscode editor
+2. node js installed
+3. npm installed
+
+### steps
+
+1. > clone this project
+2. > go into to the the project folder
+3. > open the terminal
+
+4. > open vscode using following command
+
+```sh
+$ code .
+```
+
+5. > Install the dependencies
+
+```sh
+$ npm install
+
+```
+
+6. > run the project
+
+```sh
+$ npm run start
+```
---
+++ app.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const fs = require("fs");
+
+const express = require("express");
+
+const PORT = process.env.PORT || 3000;
+const fileName = path.join(__dirname, "DATA", "blog.json");
+const data = JSON.parse(fs.readFileSync(fileName, "utf-8"));
+
+const app = express();
+app.use(express.json());
+
+app.get("/blogs/:id", (req, res) => {
+  if (req.params.id) {
+    const blog = data.find((user) => {
+      return user.id == req.params.id;
+    });
+    if (blog) {
+      res.status(200).json({
+        status: "sucessfull",
+        data: blog,
+      });
+    } else {
+      res.status(200).json({
+        status: "sucessfull",
+        data: "blog not found",
+      });
+    }
+  }
+});
+
+app.get("/blogs", (req, res) => {
+  res.json(data);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening at port ${PORT}`);
+});
