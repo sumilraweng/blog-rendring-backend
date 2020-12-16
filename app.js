8db71251147764f7217477a18909c9d33e81b252
sumilraweng <sumil.jain@raweng.com>
2020-12-16 19:25:40 +0530

    added the h1 tag and changes the link

--- app.js
+++ app.js
@@ -12,8 +12,11 @@ const app = express();
 //Using the middleware
 app.use(urlencoded({ extended: false }));
 app.use(express.json());
-
 app.use("/api/blogs", blogRouter);
+
+app.get("/", (req, res) => {
+  res.send(`<h1> Api to fetch the blogs</h1>`);
+});
 
 app.listen(PORT, () => {
   console.log(`Listening at port ${PORT}`);
