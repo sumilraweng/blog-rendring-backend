670763804439734583362bc71a8cb0a4af3339d7
sumilraweng <sumil.jain@raweng.com>
2020-12-16 19:41:02 +0530

    changes in tags

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use("/api/blogs", blogRouter);
 
 app.get("/", (req, res) => {
-  res.send(`<h1> Api to fetch the blogs</h1>`);
+  res.send("<h1> Api to fetch the blogs</h1>");
 });
 
 app.listen(PORT, () => {
